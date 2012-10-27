2e2073bcac81ceacef12eeec441fd5a76c1c3783
Radek Piekarz <radziupiekarz@gmail.com>
2012-10-27 23:20:08 +0200

    Added test project.

---
+++ src/NeuronalNetworks/NeuronalNetwork.Tests/SerializationTests.cs
@@ -0,0 +1,14 @@
+﻿using NUnit.Framework;
+
+namespace NeuronalNetwork.Tests
+{
+    [TestFixture]
+    public class SerializationTests
+    {
+        [Test]
+        public void TestMethod1()
+        {
+            //to do ;-)
+        }
+    }
+}
