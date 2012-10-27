e4fe8d8bccfbdd129d7cafaa747fe8376a0a575b
Radek Piekarz <radziupiekarz@gmail.com>
2012-10-27 23:04:15 +0200

    Refactoring (added Serliazer Class)

--- src/NeuronalNetworks/NeuronalNetworks.App/Program.cs
+++ src/NeuronalNetworks/NeuronalNetworks.App/Program.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Text;
 using System.Xml.Serialization;
 using NeuronalNetworks.ActivationFunctions;
+using NeuronalNetworks.Common;
 using NeuronalNetworks.Networks;
 
 namespace NeuronalNetworks.App
@@ -14,28 +15,21 @@ namespace NeuronalNetworks.App
         static void Main(string[] args)
         {
 
-//            ActivationNetwork network = new ActivationNetwork(
-//                    new ThresholdFunction(), // sigmoid activation function
-//                    2,                      // 2 inputs
-//                    1);                 // 1 layer with 2 nerons.
-//
-//            network[0][0].Threshold = -0.73;
-//            network[0][0][0] = 0.57;
-//            network[0][0][1] = 0.38;
-//
-//            System.Xml.Serialization.XmlSerializer x = new System.Xml.Serialization.XmlSerializer(typeof(Network));
-//
-//
-//            TextWriter tw = new StreamWriter("network.xml");
-//            
-//            x.Serialize(tw, network);
-//            tw.Close(); 
+            var network = new ActivationNetwork(
+                    new ThresholdFunction(), // sigmoid activation function
+                    2,                      // 2 inputs
+                    1);                 // 1 layer with 1 neuron.
 
-            XmlSerializer deserializer = new XmlSerializer(typeof(Network));
-            TextReader textReader = new StreamReader(@"network.xml");
+            network[0][0].Threshold = -0.73;
+            network[0][0][0] = 0.57;
+            network[0][0][1] = 0.38;
 
-            var network2 = (Network)deserializer.Deserialize(textReader);
-            textReader.Close();
+            NeuronalNetworkSerializer.SerializeToXml(network, @"network.xml");
+
+
+
+            var network2 = NeuronalNetworkSerializer.DeserializeFromXml(@"network.xml");
+
 
             Console.WriteLine(network2.Compute(new double[] {0.1, 0.1})[0]);
             Console.WriteLine(network2.Compute(new double[] { 1, 1 })[0]);
---
+++ src/NeuronalNetworks/NeuronalNetworks/Common/NeuronalNetworkSerializer.cs
@@ -0,0 +1,30 @@
+﻿using System.IO;
+using System.Xml.Serialization;
+using NeuronalNetworks.Networks;
+
+namespace NeuronalNetworks.Common
+{
+    public class NeuronalNetworkSerializer
+    {
+        public static Network DeserializeFromXml(string path)
+        {
+            var deserializer = new XmlSerializer(typeof(Network));
+            using(TextReader textReader = new StreamReader(path))
+            {
+                var network = (Network) deserializer.Deserialize(textReader);
+                textReader.Close();
+                return network;
+            }
+        }
+
+        public static void SerializeToXml(Network network, string path)
+        {
+            var serializer = new XmlSerializer(typeof(Network));
+            using (TextWriter textWriter = new StreamWriter(path))
+            {
+                serializer.Serialize(textWriter, network);
+                textWriter.Close();
+            }
+        }
+    }
+}
